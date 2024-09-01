e49d5b43e35229765140ea521ca012bbfdcef58d
Delete Prod <martynjamesoconnor@hotmail.co.uk>
2024-09-01 19:59:11 +0100

    FFS

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
 resource "aws_instance" "phobos" {
   ami           = var.ami_id
   instance_type = var.instance_type
